f1a5fc792cd2051a84d684b1fda9b4b9e0dced64
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-10 10:54:59 +0530

    [Afrath] Updated the file with creating BST

---
+++ BST/BSTOperations.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST
+{
+    public class BSTOperations<T> where T : IComparable
+    {
+        public T data;
+        public BSTOperations<T> left = null, right = null;
+        public BSTOperations (T data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+        public void Insert(T data)
+        {
+            T current = this.data;
+            if (current.CompareTo(data) > 0)
+            {
+                if(this.left == null)
+                {
+                    this.left = new BSTOperations<T>(data);
+                }
+                else {
+                    this.left.Insert(data);
+                }
+            }
+            else
+            {
+                if (this.right == null)
+                {
+                    this.right = new BSTOperations<T>(data);
+                }
+                else
+                {
+                    this.right.Insert(data);
+                }
+
+            }
+        }
+        public void Display()
+        {
+            
+            if (this.left != null)
+            {
+                this.left.Display();
+            }
+            Console.Write(this.data.ToString()+" ");
+            if (this.right != null)
+            {
+                this.right.Display();
+            }
+            
+        }
+        
+            
+
+    }
+}
--- BST/Program.cs
+++ BST/Program.cs
@@ -7,6 +7,12 @@ namespace BST
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to BST");
+            BSTOperations<int> bST = new BSTOperations<int>(56);
+            bST.Insert(30);
+            bST.Insert(70);
+            Console.WriteLine("Display using inorder traversal:");
+            bST.Display();
+            Console.WriteLine(" ");
         }
     }
 }
